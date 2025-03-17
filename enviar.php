c62f10ffd58c669ff46d0202794e0e1959b203af
laucadiz <laura.cadiz@gmail.com>
2025-03-17 17:08:49 -0300

    Ejercicio 01 Formulario

---
+++ enviar.php
@@ -0,0 +1,13 @@
+<?php
+    /* <html>
+    <body>
+        <h1>Hola</h1>
+    </body>
+    </html> */
+?>
+
+<?php
+    echo $_GET['nombre'];
+    echo $_GET['apellido'];
+    echo $_GET['email'];
+?>
